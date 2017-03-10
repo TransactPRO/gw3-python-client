713d3b1438806ec856f0cced9c1a974222c28bea
Artjom Nemiro <artjom.nemiro@transactpro.lv>
2017-03-10 12:22:23 +0200

    Update default config to real API url and tiemot 60 sec

--- gateway/__init__.py
+++ gateway/__init__.py
@@ -5,11 +5,11 @@ Transact Pro Payment Gateway integration library.
 from gateway.client import Client
 
 # Transact PRO API configuration
-API_BASE_URL = 'http://uriel.sk.fpngw3.env/'
+API_BASE_URL = 'https://api.transactpro.lv/v3.0'
 API_VERSION = 'v3.0'
 
 # Transact PRO HTTP transport configuration
-HTTP_TIME_OUT = 10
+HTTP_TIME_OUT = 60
 HTTP_VERIFY_SSL_CERTS = True
 # Example: { 'http': 'http://<user>:<pass>@<proxy>:<port>', 'https': 'http://<user>:<pass>@<proxy>:<port>' }
 HTTP_PROXY = None
